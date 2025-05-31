2e1d147847ff6523785e5962bafa58c94872391c
Eshemeidomi Samuel Oghenevwede <eshemiedomisamuelb@gmail.com>
2025-05-31 13:15:18 +0100

    updated auth implementation, added password and email check

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from core.extensions import db
+from core.extensions import db, flask_bcrypt
 from datetime import datetime
 
 class User(db.Model):
@@ -9,6 +9,17 @@ class User(db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
     subscriptions = db.relationship('Subscription', backref='user', lazy=True)
+
+    @property
+    def password(self):
+        raise AttributeError('password: write-only field')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return flask_bcrypt.check_password_hash(self.password_hash, password)
 
     
 class Plan(db.Model):
@@ -27,4 +38,12 @@ class Subscription(db.Model):
     is_active = db.Column(db.Boolean, default=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     plan_id = db.Column(db.Integer, db.ForeignKey('plan.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+
+    def is_current(self):
+        return self.is_active and self.end_date > datetime.now()
+
+    def cancel(self):
+        self.is_active = False
+        self.end_date = datetime.now()
 
